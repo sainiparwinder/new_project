2ea10d1e439697674a7e8097d8371f0166194b6f
sainiparwinder <47318532+sainiparwinder@users.noreply.github.com>
2019-02-04 22:52:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+// programm to upload a file in any folder........
+
+      if(!empty($_POST['submit']))
+      {
+           //print_r( $_FILES['file']);
+           $filename= $_FILES[file][name];
+           $filetype= $_FILES[file][type];
+           $temp_load= $_FILES[file][tmp_name];
+           $file_size= $_FILES[file][size];
+           $file_target="new_project/".$filename;
+
+          if(move_uploaded_file($temp_load, $file_target))
+          {
+	          echo "file is uploaded";
+          }
+          else
+          {
+			  echo "file is not loaded";
+		   }	  
+       }
+
+?>
+<html><head><title>file uploads</title></head>
+  <body>
+         <form action="?" method="post" enctype="multipart/form-data">
+              <input type="file" name="file">
+              <input type ="submit" name="submit">
+         </form>
+  </body>
+
+</html>
