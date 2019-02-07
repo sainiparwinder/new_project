e63db816b657850cb72d2a1200cb2c81e09d56a2
Satwinder Singh <satwinder.singh.21@gmail.com>
2019-02-07 13:35:00 +0530

    index file

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+function pr($a = []){ echo "<pre>"; print_r($a); echo "</pre>"; }
+
 // programm to upload a file in any folder........
 
 // git status
@@ -7,21 +9,18 @@
 //git commit - m 'commit to local repositery'
 //git push origin master
 
-      if(!empty($_POST['submit']))
-      {
-           //print_r( $_FILES['file']);
-           $filename= $_FILES[file][name];
-           $filetype= $_FILES[file][type];
-           $temp_load= $_FILES[file][tmp_name];
-           $file_size= $_FILES[file][size];
-           $file_target="new_project/".$filename;
-
-          if(move_uploaded_file($temp_load, $file_target))
-          {
+      if(!empty($_POST['submit'])) {
+		  pr("testtsttets");
+		  pr($_FILES); die;
+           $filename= $_FILES['myfile']['name'];
+           //$filetype= $_FILES[myfile][type];
+           $temp_load= $_FILES['myfile']['tmp_name'];
+          // $file_size= $_FILES[myfile][size];
+           $file_target = __DIR__."/".$filename;
+			 
+          if(move_uploaded_file($temp_load, $file_target)) {
 	          echo "file is uploaded";
-          }
-          else
-          {
+          } else {
 			  echo "file is not loaded";
 		   }	  
        }
@@ -30,7 +29,7 @@
 <html><head><title>file uploads</title></head>
   <body>
          <form action="?" method="post" enctype="multipart/form-data">
-              <input type="file" name="file">
+              <input type="file" name="myfile">
               <input type ="submit" name="submit">
          </form>
   </body>
